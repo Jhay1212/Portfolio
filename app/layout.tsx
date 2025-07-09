2db30208e4adea6103c611b92ec403eebac5f453
Jhay12 <jhayjane78@gmail.com>
2025-07-09 08:59:07 +0800

    alksdjakldj

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import ThreeD from "./ThreeD";
+// asdasd
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
